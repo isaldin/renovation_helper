497a48ceacb43ef3fc956492fd156f9458b0e201
ilya saldin <saldin.iliya@gmail.com>
2025-06-03 13:50:20 +0500

    feat: finish collapseItem component

--- web/src/components/RhCollapseItem.vue
+++ web/src/components/RhCollapseItem.vue
@@ -2,31 +2,49 @@
   <div class="rh-collapse-item">
     <slot name="left" />
 
-    <template v-if="$slots['default']">
-      <div class="rh-collapse-item__content">
-        <slot />
-      </div>
-    </template>
+    <div class="rh-collapse-item__title" :class="{ 'rh-collapse-item__title--expanded': expanded }">
+      <template v-if="$slots['default']">
+        <div class="rh-collapse-item__content">
+          <slot />
+        </div>
+      </template>
 
-    <slot v-if="hasCollapsableContent" name="right">
-      <div>icon</div>
-    </slot>
+      <slot v-if="hasCollapsableContent" name="right">
+        <slot v-if="!expanded" name="expandIcon" :toggle-collapse="toggleCollapse" />
+        <slot v-else name="collapseIcon" :toggle-collapse="toggleCollapse" />
+      </slot>
+    </div>
 
-    <slot name="collapsableContent" />
+    <slot v-if="expanded" name="collapsableContent" />
   </div>
   <n-divider />
 </template>
 
 <script setup lang="ts">
 import { NDivider } from 'naive-ui';
-import { useSlots } from 'vue';
+import { useSlots, ref } from 'vue';
 
 const hasCollapsableContent = !!useSlots()['collapsableContent'];
+
+const expanded = ref(false);
+
+const toggleCollapse = () => {
+  expanded.value = !expanded.value;
+};
 </script>
 
 <style lang="scss" scoped>
 .rh-collapse-item {
   display: flex;
+  flex-direction: column;
+
+  &__title {
+    display: flex;
+
+    &--expanded {
+      min-height: 48px;
+    }
+  }
 
   &__content {
     flex: 1;
