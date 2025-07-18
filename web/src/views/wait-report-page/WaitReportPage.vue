649b1b5237b868bddeef6a87dc02615fdfdcee34
ilya saldin <saldin.iliya@gmail.com>
2025-07-18 22:37:00 +0500

    feat(web): add "To bot" button to wait-report screen

--- web/src/views/wait-report-page/WaitReportPage.vue
+++ web/src/views/wait-report-page/WaitReportPage.vue
@@ -3,12 +3,22 @@
     <message-page-layout>
       <template #title> Ожидание отчета </template>
       <template #text> Вы можете закрыть приложение, отчет придет в бот. </template>
+
+      <div class="mt-4">
+        <n-button type="primary" size="large" @click="handleCloseApp"> К боту </n-button>
+      </div>
     </message-page-layout>
   </div>
 </template>
 
 <script setup lang="ts">
 import MessagePageLayout from '@app/views/MessagePageLayout.vue';
+import { NButton } from 'naive-ui';
+import { getTelegramWebAppService } from '@app/container';
+
+const handleCloseApp = () => {
+  getTelegramWebAppService().closeApp();
+};
 </script>
 
 <style lang="scss" scoped>
